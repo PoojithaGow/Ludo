1740f9d0c6302ae30a22c0ef93b5e5b7def1a431
PoojithaGow <poojithamg935@gmail.com>
2021-12-13 11:39:58 +0530

    1st commit

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public static int numberOfPlayers;
+
+    public void TwoPlayers()
+    {
+        SoundManager.buttonAudioSource.Play();
+        numberOfPlayers = 2;
+        SceneManager.LoadScene("Ludo");
+    }
+
+    public void FourPlayers()
+    {
+        SoundManager.buttonAudioSource.Play();
+        numberOfPlayers = 4;
+        SceneManager.LoadScene("Ludo");
+    }
+
+    public void Quit()
+    {
+        SoundManager.buttonAudioSource.Play();
+        if (EditorApplication.isPlaying == true)
+        {
+            EditorApplication.isPlaying = false;
+        }
+    }
+}
