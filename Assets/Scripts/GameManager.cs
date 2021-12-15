81b2e5b4f13860d884770ef47f50526bd60c31ac
PoojithaGow <poojithamg935@gmail.com>
2021-12-15 11:59:38 +0530

    Added Exit button functionality

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -59,11 +60,110 @@ public class GameManager : MonoBehaviour
 
     private System.Random randomNum;
 
+    public void Exit()
+    {
+        SoundManager.buttonAudioSource.Play();
+        confirmScreen.SetActive(true);
+    }
+
+    public void ExitYesButton()
+    {
+        SoundManager.buttonAudioSource.Play();
+        SceneManager.LoadScene("GameMenu");
+    }
+     public void ExitNoButton()
+    {
+        SoundManager.buttonAudioSource.Play();
+        confirmScreen.SetActive(false);
+    }
+
+    /*IEnumerator GameOver
+    {
+        yield return new WaitForSeconds(1.5f);
+        gameOverScreen.SetActive(true);
+    
+    }*/
+
+    public void GameOverYesButton()
+    {
+        SoundManager.buttonAudioSource.Play();
+        SceneManager.LoadScene("GamePlay");
+    }
+
+    public void GameOverNoButton()
+    {
+        SoundManager.buttonAudioSource.Play();
+        SceneManager.LoadScene("GameMenu");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        QualitySettings.vSyncCount = 1;
+        Application.targetFrameRate = 30;
+
+        randomNum = new System.Random();
+        for (int i = 0; i <= 5; i++)
+        {
+            diceAnimations[i].SetActive(false);
+        }
+
+        //Set initial position for players
+        for (int i = 0; i <= 3; i++)
+        {
+            redPlayersPos[i] = redPlayers[i].transform.position;
+            bluePlayersPos[i] = bluePlayers[i].transform.position;
+            greenPlayersPos[i] = greenPlayers[i].transform.position;
+            yellowPlayersPos[i] = yellowPlayers[i].transform.position;
+        }
+
+        //Deactivivating player borders
+        for (int i = 0; i <= 3; i++)
+        {
+            redBorder[i].SetActive(false);
+            blueBorder[i].SetActive(false);
+            greenBorder[i].SetActive(false);
+            yellowBorder[i].SetActive(false);
+        }
+
+        //Deactivivating winning screen
+        for (int i = 0; i <= 3; i++)
+        {
+            individualWinningScreen[i].SetActive(false);
+        }
+
+        switch (MainMenuManager.numberOfPlayers)
+        {
+            case 2:
+                playerTurn = "RED";
+                frames[0].SetActive(true);
+                /*for (int i = 1; i <= 3; i++)
+                {
+                    frames[i].SetActive(false);
+                }*/
+                for (int i = 0; i <= 3; i++)
+                {
+                    bluePlayers[i].SetActive(false);
+                    yellowPlayers[i].SetActive(false);
+                }
+                break;
+
+            case 4:
+                playerTurn = "RED";
+                frames[0].SetActive(true);
+                /*for (int i = 1; i <= 3; i++)
+                {
+                    frames[i].SetActive(false);
+                }*/
+                diceButtonPos.position = eachDicePos[0].position;
+                break;
+        }
     }
+
+
+    
+
+   
 
     // Update is called once per frame
     void Update()
